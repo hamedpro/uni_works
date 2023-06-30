ba0616cc4b579e467bf28ed3c449169e0987dbed
hamed yaghoutpour <hamedpro30@gmail.com>
2023-06-30 14:20:54 +0330

    working on t2-hw4-task4 animations and an algo prob

--- term2/test2.py
+++ term2/test2.py
@@ -13,18 +13,9 @@ pca_model.fit(onehot_encoded_df)
 
 df_2d = pd.DataFrame(pca_model.transform(onehot_encoded_df))
 
-
 def kmeans(df, k, max_iteration=200):
     # df must be 2d with these columns : 0, 1
     def find_mean_of_points(points):
-        for item in [point[0] for point in points]:
-            if not isinstance(item , int) and not isinstance(item , float):
-                print(item )
-        
-        for item in [point[1] for point in points]:
-            if not isinstance(item , int) and not isinstance(item , float):
-                print(item )
-        
         return [
             np.mean([point[0] for point in points]),
             np.mean([point[1] for point in points]),
@@ -54,24 +45,18 @@ def kmeans(df, k, max_iteration=200):
             raise "Couldnt assign a centroid to a point : two centroids have the same distance with this point"
         index_of_max = distances.index(max(distances))
         return centroids[index_of_max]
-    
-    centroids = [
-        np.random.choice(points)
-        for i in range(k)
-    ]
 
-    """ instead of choice(x) -> [
+    centroids = [[
         np.random.randint(min(x_values), max(x_values) + 1),
         np.random.randint(min(y_values), max(y_values) + 1),
-    ] """
+    ] for _ in range(k )]
     plt.scatter(x_values, y_values, color="blue")
     plt.scatter(
         [centroid[0] for centroid in centroids],
         [centroid[1] for centroid in centroids],
         color="green",
     )
-
-    for i in range(max_iteration):
+    for _ in range(max_iteration):
         labeled_points = [
             {
                 "point": point,
@@ -90,8 +75,7 @@ def kmeans(df, k, max_iteration=200):
             for labeled_point in labeled_points:
                 if labeled_point["nearest_centroid"] == centroid:
                     points_of_centroid.append(labeled_point["point"])
-            centroids.append(find_mean_of_points(points_of_centroid))
-
+            centroids.append(find_mean_of_points(points_of_centroid) if len(points_of_centroid) != 0 else centroid)
         plt.scatter(
             list(map(lambda centroid: centroid[0], centroids)),
             list(map(lambda centroid: centroid[1], centroids)),
@@ -100,7 +84,7 @@ def kmeans(df, k, max_iteration=200):
         plt.pause(0.0001)
         if prev_centroids == centroids:
             break
-    plt.show()
+    #plt.show()
 
 
 kmeans(df_2d, 3, max_iteration=10)
