374f79b8a6f04a72b501667759c873176e01b06a
hamed yaghoutpour <hamedpro30@gmail.com>
2023-07-01 17:37:10 +0330

    merged sudoku gui and backend

--- term2/sudoku.py
+++ term2/sudoku.py
@@ -1,1 +1,179 @@
+import tkinter
 import numpy as np 
+from tkinter import filedialog
+import json
+# code is forked from : https://github.com/kubicodes/Backtracking-Sudoku-Solver-with-Python.git
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("------------------------")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty_position(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+
+    return None
+
+def is_Valid(board, position, number):
+    # in that row and col and cube that position is located in there is not any
+    # other cell with number= that number we want to put in this position 
+    
+    # check row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and position[1] != i:
+            return False
+
+    #check column
+    for i in range(len(board[0])):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    #check cube
+    cube_x = position[1] // 3
+    cube_y = position[0] // 3
+
+    for i in range(cube_y * 3, cube_y * 3 + 3):
+        for j in range(cube_x * 3, cube_x * 3 + 3):
+            if board[i][j] == number and (i,j) != position:
+                return False
+
+    return True
+def validate_board(board):
+    #todo check this algo 
+    for i in range(9) :
+        for j in range(9 ) : 
+            if board[i][j] != 0 :
+                if not is_Valid(board , [i,j], board[i][j]):
+                    return False
+    return True 
+def solve_board(board):
+    empty_position = find_empty_position(board)
+
+    #base case
+    if not empty_position:
+        return True
+    else:
+        row, col = empty_position
+
+    for i in range(1, 10):
+        if is_Valid(board, (row, col), i):
+            board[row][col] = i
+
+            #backtrack and try again
+            if solve_board(board):
+                return True
+            board[row][col] = 0
+
+    return False
+
+def gen_random_board(number_of_empty_cells=30):
+    board = [np.zeros(9) for i in range(9)]
+    board[np.random.randint(9)][np.random.randint(9)] = np.random.randint(1,10)
+    solve_board(board)
+
+    cells_to_empty = []
+    def gen_random_cell():
+        return [np.random.randint(9),np.random.randint(9)]
+
+    #add `number_of_empty_cells` unique cells to cells_to_empty 
+    for i in range(number_of_empty_cells):
+        while True :
+            t = gen_random_cell()
+            if not t in cells_to_empty:
+                cells_to_empty.append(t)
+                break
+    for cell in cells_to_empty:
+        
+        board[cell[0]][cell[1]] = 0 
+    
+
+    return board
+
+ 
+class GUI:
+    @property
+    def labels_data(self):
+        t = [[] for i in range(9) ]
+        for i in range(9):
+            for j in range(9):
+                t[i].append(self.entry_controlled_variables[i * 9 + j ].get())
+        return t 
+    @labels_data.setter
+    def labels_data(self , new_value ):
+        for row in range(9):
+            for col in range(9):
+                self.entry_controlled_variables[row*9 + col ].set(new_value[row][col])
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.geometry("500x500")
+        self.entry_controlled_variables = [tkinter.IntVar(self.window , 0) for i in range(81)]
+        #creating label components 
+        self.labels = [ ]
+        for i in range(81):
+            l = tkinter.Label(self.window)
+            cor = self.index_to_cordinates(i)
+            
+            l.grid(row = cor[0],column=cor[1])
+            self.labels.append(l)
+
+        #create difficulty radio buttons
+        self.radio_buttons_variable = tkinter.IntVar(self.window,30)
+        
+        t = tkinter.Radiobutton(self.window,text="easy" , value=30, variable=self.radio_buttons_variable)
+        t.grid(row = 10,column=1)
+        t = tkinter.Radiobutton(self.window,text="medium" , value=40, variable=self.radio_buttons_variable)
+        t.grid(row = 10,column=2)
+        t = tkinter.Radiobutton(self.window,text="hard" , value=50, variable=self.radio_buttons_variable)
+        t.grid(row = 10,column=3)
+
+        
+        #create control menu
+        t = tkinter.Button(self.window,text="create",command=self.create)
+        t.grid(row = 11,column=1)
+        t = tkinter.Button(self.window,text="load",command=self.load)
+        t.grid(row = 11,column=2)
+        t = tkinter.Button(self.window,text="solve",command=self.solve)
+        t.grid(row = 11,column=3)
+        t = tkinter.Button(self.window,text="check",command=self.check)
+        t.grid(row = 11,column=4)
+        self.update()
+        self.window.mainloop()
+    def load(self):
+        filename = filedialog.askopenfilename(title="select a file")
+        new_labels_data = []
+        for line in open(filename , 'r'):
+            new_labels_data.append([int(cell) for cell in line.split(',')])
+        self.labels_data = new_labels_data
+        self.update()
+    def create(self):
+        self.labels_data =gen_random_board(self.radio_buttons_variable.get())
+        self.update()   
+    def solve(self):
+        solve_board(self.labels_data)
+        self.update()
+    def check(self):
+        if validate_board(self.labels_data) == True :
+            tkinter.messagebox.showinfo(title = 'board validation result' , message= 'board is valid')
+        else:
+            tkinter.messagebox.showerror(title='board validation result' , message= 'board is invalid')
+    def index_to_cordinates(self, index):
+        return [index // 9, index % 9]
+
+    def update(self):
+        for i in range(81):
+            cor = self.index_to_cordinates(i)
+            self.labels[i].config(text=self.labels_data[cor[0]][cor[1]])
+
+    
+        
+GUI()
--- term2/test2.py
+++ term2/test2.py
@@ -1,4 +1,60 @@
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.decomposition import PCA
+
+# Question 4)))))
+
+# Set the random seed for reproducibility
 np.random.seed(42)
-for i in range(20):
-    print(np.random.random(3))
+
+# Read in the dataset and perform one-hot encoding
+df = pd.read_csv("Dry_Bean.csv")
+onehot_encoded_df = pd.get_dummies(df)
+
+
+# Use PCA to reduce the dimensionality of the dataset to 2 features
+pca_model = pd.DataFrame(PCA(n_components=2).fit_transform(onehot_encoded_df))
+
+
+# Define the k-means clustering function
+def kmeans(X, k, epsilon=1e-5):
+    centroids = X[np.random.choice(len(X), k, replace=False)]
+    fig, ax = plt.subplots()
+    counter = 0
+    prev_clusters = np.zeros(X.shape[0])
+    while True:
+        # Assign each data point to the nearest centroid
+        clusters = np.argmin(
+            np.linalg.norm(X[:, np.newaxis] - centroids, axis=2), axis=1
+        )
+        # Update the position of the centroids
+        new_centroids = np.array([X[clusters == j].mean(axis=0) for j in range(k)])
+        # Check for convergence
+        if np.allclose(centroids, new_centroids, rtol=epsilon):
+            break
+        if np.array_equal(prev_clusters, clusters):
+            counter += 1
+            if counter == 5:
+                break
+        else:
+            prev_clusters = clusters
+            counter = 0
+        centroids = new_centroids
+        ax.clear()
+        ax.scatter(X[:, 0], X[:, 1], c=clusters)
+        ax.scatter(
+            centroids[:, 0],
+            centroids[:, 1],
+            marker="o",
+            s=50,
+            edgecolors="black",
+            linewidths=2,
+            c="#050505",
+        )
+        plt.pause(0.1)
+    plt.show()
+
+
+# Call the k-means clustering function
+kmeans(pca_model.values, k=5)
