5a4fe04d281433dd98ceb073da76fa466de8ff31
hamed yaghoutpour <hamedpro30@gmail.com>
2023-07-04 23:52:46 +0330

    complete melika's sudoku

--- term2/melika-sudoku.py
+++ term2/melika-sudoku.py
@@ -1,8 +1,9 @@
 import random
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
+import copy
 class SudokuBackend:
-    difficulty = 51 #it defaults to easy mode with 30 empty cells 
     def print_board(self, board):
         for i in range(len(board)):
             if i % 3 == 0 and i != 0:
@@ -18,12 +19,14 @@ class SudokuBackend:
 
     def gen_empty_grid(self):
         return [[0 for i in range(9)] for j in range(9)]
-    def generate(self):
+
+    """ difficalty is number of not empty cells """
+    def generate(self ,difficulty = 51   ): 
         grid = self.gen_empty_grid()
         grid[random.randint(0,8)][random.randint(0,8)] = random.randint(1,9)
         self.solve(grid)
 
-        count = 81 - self.difficulty
+        count = 81 - difficulty
         while count > 0:
             row, col = random.randint(0, 8), random.randint(0, 8)
             if grid[row][col] != 0:
@@ -43,7 +46,7 @@ class SudokuBackend:
             for col in range(9):
                 if grid[row][col] == 0:
                     for num in range(1, 10):
-                        if self.is_valid(grid, row, col, num):
+                        if self.can_be_put(grid, [row, col], num):
                             grid[row][col] = num
                             if self.count_solutions(grid) != 1:
                                 return 2
@@ -51,25 +54,55 @@ class SudokuBackend:
                     return 1
         return 1
 
-    def is_valid(self, grid, row, col, num):
-        # in that row and col and cube that position is located in there is not any
-        # other cell with number= that number we want to put in this position
-        for i in range(9):
-            if grid[row][i] == num:
-                return False
-        # check if the number is valid in the column
-        for i in range(9):
-            if grid[i][col] == num:
-                return False
-        # check if the number is valid in the 3x3 box
-        row_start = (row // 3) * 3
-        col_start = (col // 3) * 3
-        for i in range(row_start, row_start + 3):
-            for j in range(col_start, col_start + 3):
-                if grid[i][j] == num:
+    
+    def is_valid(self , board):
+        # check row
+        for row in board:
+            for number in range(1, 10):
+                if row.count(number) > 1:
                     return False
+
+        # check column
+        for col_index in range(9):
+            col = []
+            for row in board:
+                col.append(row[col_index])
+            for number in range(1,10):
+                if col.count(number) > 1:
+                    return False
+
+        # check cube
+        positions = [
+            [0, 0],
+            [0, 3],
+            [0, 6],
+            [3, 0],
+            [3, 3],
+            [3, 6],
+            [6, 0],
+            [6, 3],
+            [6, 6],
+        ]
+
+        for number in range(1, 10):
+            for position in positions:
+                cube_x = position[1] // 3
+                cube_y = position[0] // 3
+                cube_cells = []
+                for i in range(cube_y * 3, cube_y * 3 + 3):
+                    for j in range(cube_x * 3, cube_x * 3 + 3):
+                        cube_cells.append(board[i][j])
+                if cube_cells.count(number) > 1:
+                    return False
+
         return True
 
+    def can_be_put(self , board, position, number):
+        # in that row and col and cube that position is located in there is not any
+        # other cell with number= that number we want to put in this position
+        t = copy.deepcopy(board)
+        t[position[0]][position[1]] = number
+        return self.is_valid(t)
     def solve(self,grid):
         # find an empty cell
         for row in range(9):
@@ -77,7 +110,7 @@ class SudokuBackend:
                 if grid[row][col] == 0:
                     # try values from 1 to 9
                     for num in range(1, 10):
-                        if self.is_valid(grid, row, col, num):
+                        if self.can_be_put(grid,[ row, col], num):
                             grid[row][col] = num
                             # continue solving recursively
                             if self.solve(grid):
@@ -96,6 +129,7 @@ class SudokuGUI:
         self.create_grid()
         self.grid = self.sudoku.gen_empty_grid()
         self.create_bottom_buttons()
+        self.create_difficalty_radio_button()
         self.root.mainloop()
     @property
     def grid(self ):
@@ -139,10 +173,22 @@ class SudokuGUI:
         create_button  = Button(self.root , text="create random grid" , command=self.create_button_onclick)
         create_button.grid(row = 9 , column=  6)
 
+    def create_difficalty_radio_button(self):
+        self.radio_button_variable = IntVar(self.root , 51 )
+        t = Radiobutton(self.root , value=51 , text = 'easy' , variable= self.radio_button_variable)
+        t.grid(row = 10 , column= 1)
+
+        t = Radiobutton(self.root , value=41 , text = 'medium' , variable= self.radio_button_variable)
+        t.grid(row = 10 , column= 2)
+
+        t = Radiobutton(self.root , value=31 , text = 'hard' , variable= self.radio_button_variable)
+        t.grid(row = 10 , column= 3)
+
     #button onclick funcs 
     def create_button_onclick(self):
-        self.grid = self.sudoku.generate()
-        
+        self.grid = self.sudoku.generate(difficulty= self.radio_button_variable.get())
+    def rerender(self):
+        self.grid = copy.deepcopy(self.grid)
     def load_puzzle(self):
         filename = filedialog.askopenfilename()
         if filename == "":
@@ -154,25 +200,22 @@ class SudokuGUI:
         self.grid = new_grid
         
     def solve_puzzle(self):
-        self.sudoku.solve(self.grid)
+        if self.sudoku.is_valid(self.grid) == False:
+            self.show_message('this board is not solveable')
+            return 
+        t = copy.deepcopy(self.grid)
+
+        self.sudoku.solve(t)
+        self.grid = t 
         
     def check_solution(self):
         user_grid = self.grid 
-        if self.check_valid(user_grid):
-            self.show_message("Congratulations, you solved the puzzle!")
+        if self.sudoku.is_valid(user_grid):
+            self.show_message("continue! this puzzle can be solved")
         else:
-            self.show_message("Invalid input. Please check your entries.")
+            self.show_message("stop trying. this puzzle is not solvable")
 
-    def check_valid(self, grid):
-        for row in range(9):
-            for col in range(9):
-                if grid[row][col] != 0:
-                    num = grid[row][col]
-                    grid[row][col] = 0
-                    if not self.sudoku.is_valid(grid, row, col, num):
-                        return False
-                    grid[row][col] = num
-        return True
+    
 
     def check_solution_helper(self, grid1, grid2):
         for row in range(9):
